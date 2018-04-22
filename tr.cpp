642998e5f8dc82b02d0356c6067b2453b3c37286
Grigorii-Ilin <grigory1728@gmail.com>
2018-04-22 10:58:29 +0300

    first commit

---
+++ tr.cpp
@@ -0,0 +1,70 @@
+﻿/*
+We need to transpose a matrix
+Транспонирование матрицы
+*/
+
+#include "stdafx.h"
+
+
+#include <conio.h>
+#include <locale.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+#pragma warning(disable:4996)
+
+
+
+
+int main()
+{
+
+	setlocale(LC_ALL, "rus");
+
+
+	const int N = 100; //макс. размер матрицы
+	int m[N][N];
+	int r = 0, c = 0;
+
+	printf("\n Введите количество строк и столбцов матрицы: ");
+	scanf("%d%d", &r, &c);
+
+	srand((unsigned)time(NULL));
+
+	printf("\n Исходная матрица: ");
+	int i, j;
+	for (i = 0; i <= r - 1; i++)
+	{
+		printf("\n\t");
+		for (j = 0; j <= c - 1; j++)
+		{
+			m[i][j] = rand() % 50;
+			printf("%5d", m[i][j]);
+		}
+	}
+
+	int t[N][N];//  транспонированная матрица
+	for (i = 0; i <= r - 1; i++)
+	{
+		for (j = 0; j <= c - 1; j++)
+		{
+			t[j][i] = m[i][j];
+		}
+	}
+
+	printf("\n Транспонированная матрица: ");
+	for (i = 0; i <= c - 1; i++)
+	{
+		printf("\n\t");
+		for (j = 0; j <= r - 1; j++)
+		{
+			printf("%5d", t[i][j]);
+		}
+	}
+
+
+	getch();
+	return 0;
+}
+
